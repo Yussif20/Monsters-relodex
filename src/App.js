35e7c0fcc8819f6c2546cd71888ea5ccfa07ee84
Yussif20 <yousefayman2000500@GMAIL.COM>
2022-06-24 05:08:53 +0200

    getting the monsters data from an API

--- src/App.js
+++ src/App.js
@@ -6,31 +6,33 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "yousef", lastName: "ayman" },
-      company: "ztm",
+      Monsters: [],
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { Monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello,{this.state.name.firstName} {this.state.name.lastName} I work
-            at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(() => {
-                return {
-                  name: { firstName: "andrei", lastName: "yuhan" },
-                };
-              });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.Monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
