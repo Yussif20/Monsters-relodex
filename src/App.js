c09448134c503bd9d9be27cf238c4f05f35f4774
Yussif20 <yousefayman2000500@GMAIL.COM>
2022-06-26 22:45:09 +0200

    card list component modified

--- src/App.js
+++ src/App.js
@@ -39,16 +39,7 @@ class App extends Component {
           type="search"
           onChange={onSearchChange}
         />
-        {/*
-       {filteredMonsters.map((monster) => {
-         return (
-           <div key={monster.id}>
-           <h1>{monster.name}</h1>
-           </div>
-           );
-          })}
-        */}
-        <CardList />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
