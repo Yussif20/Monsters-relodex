736f5f3911ddf88d2e4c0b5767459953c8448121
Yussif20 <yousefayman2000500@GMAIL.COM>
2022-06-27 23:01:35 +0200

    rendering monsters and using useEffects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
@@ -6,11 +6,28 @@ import "./App.css";
 
 const App = () => {
   const [searchField, setSearchField] = useState(""); //[value,serValue]
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  console.log(`render`);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
+
   return (
     <div className="App">
       <h1 className="app-title">Monster Relodex</h1>
@@ -19,10 +36,7 @@ const App = () => {
         className="monsters-search-box"
         placeholder="search monsters"
       />
-
-      {/* 
-     <CardList monsters={filteredMonsters} />
-    */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
